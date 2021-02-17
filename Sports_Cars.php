8f1a991ebdfdfb82d4953e48b85e98c8ae7d0692
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-02-17 13:48:42 -0800

    Edited the titles of the 3 main pages

--- Sports_Cars.php
+++ Sports_Cars.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Cult-Classic Japanese Cars</title>
+        <title>Cult-Classic Japanese Cars: Sports Cars</title>
         <link rel = "stylesheet" href = "style.css">
     </head>
 
