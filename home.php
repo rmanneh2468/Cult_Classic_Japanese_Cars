0c5c203594898123892618f0669505338fcf44b9
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-02-17 02:16:14 -0800

    Category page

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cult-Classic Japanese Cars</title>
+        <link rel = "stylesheet" href = "style.css">
+    </head>
+
+    <body>
+        <h1>Sportcar</h1>
+
+        <br></br>
+        
+        <h2>Cult-Classic Japanese Cars</h2>
+
+        <br></br>
+
+        <p>Welcome to Sportcar page 
+        </p>
+        
+        <br></br>
+      <!--  <ol>
+            <li> <a href = "home.php"> Sports Cars </a></li>
+            <li> <a href = "home.php"> GT Cars </a></li>
+            <li> <a href = "home.php"> Luxury Cars </a></li>
+        </ol> -->
+        <br></br>
+
+        <p> 
+            Please click on one of the three main sections above, to discover more, 
+            about these classic, special cars.
+        </p>
+
+    </body>
+</html>
