b0446cc6ee5e95aae73a15fe134ae85c3aed0957
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-02-17 21:00:40 -0800

    Changed the css files at the top of the main category sites, and the specific car site

--- Luxury_Cars.php
+++ Luxury_Cars.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Cult-Classic Japanese Cars: Luxury Cars</title>
-        <link rel = "stylesheet" href = "style.css">
+        <link rel = "stylesheet" href = "main_cat_style.css">
     </head>
 
     <body>
