745eb193f46c782ad805a69f4f41244680e33874
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-02-17 19:45:16 -0800

    Create Nissan 240SX (S14).php

---
+++ Nissan 240SX (S14).php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cult-Classic Japanese Cars: Sports Cars</title>
+        <link rel = "stylesheet" href = "style.css">
+    </head>
+
+    <body>
+        <h1>1995-1998 Nissan 240sx (S14)</h1>
+
+
+        <br></br>
+
+        <p>Description:</p> 
+        <p>The Nissan 240sx was an agile, lightweight, Rear-Wheel Drive sports car. The car was
+        available, with a 5-speed manual transmission. Due to all of these factors, it gained
+        a large following of spirited fans, in the racing, autocross, and drifting communities.
+        The car holds a legendary status, in the communities, of fans of 90's Japanese sports 
+        cars. In fact, it is such a popular and desired car, that it is nearly impossible to find 
+        a used 240sx, that has not been abused by a racer, or modded extensively. 
+        </p>
+        
+        <br></br>
+      <!--  <ol>
+            <li> <a href = "home.php"> Sports Cars </a></li>
+            <li> <a href = "home.php"> GT Cars </a></li>
+            <li> <a href = "home.php"> Luxury Cars </a></li>
+        </ol> -->
+        <br></br>
+
+        <a href = "Sports_Cars.php"> Return to the Sports Cars page </a>
+        <a href = "index.php"> Return Home </a>
+
+
+
+    </body>
+</html>
