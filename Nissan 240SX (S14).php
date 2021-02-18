4938f79b30589daf35e2106b84f9f1f19ac9b64d
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-02-17 19:55:57 -0800

    Update Nissan 240SX (S14).php

--- Nissan 240SX (S14).php
+++ Nissan 240SX (S14).php
@@ -8,13 +8,14 @@
     <body>
         <h1>1995-1998 Nissan 240sx (S14)</h1>
 
+        <img src = https://www.nydailynews.com/resizer/HzGbovJ7FeEYRkTVJn3WANh4XIg=/800x531/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/QUFM7L7IJ3HMVE3RH6XK752POI.jpg />
 
         <br></br>
 
         <p>Description:</p> 
         <p>The Nissan 240sx was an agile, lightweight, Rear-Wheel Drive sports car. The car was
         available, with a 5-speed manual transmission. Due to all of these factors, it gained
-        a large following of spirited fans, in the racing, autocross, and drifting communities.
+        a large following, of spirited fans, in the racing, autocross, and drifting communities.
         The car holds a legendary status, in the communities, of fans of 90's Japanese sports 
         cars. In fact, it is such a popular and desired car, that it is nearly impossible to find 
         a used 240sx, that has not been abused by a racer, or modded extensively. 
