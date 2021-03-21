6e72716d8e6356c90b808742ec1f2181757d15eb
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-03-21 01:36:00 -0700

    Work on Logo

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <link rel = "stylesheet" href = "index_style.css">
         <link rel = "icon" href ="Images/car-64-215428.png" type ="image/png" />  <!-- This line is for the favicon. It took me a while for it to work, and I want to improve the clarity, will work on it later. -->
         <div class = "logo_ccjc">
-            <img src = "Images/Logo_4.jpg">
+            <img src = "Images/Logo_diff_2.jpg">
         </div>
 
     </head>
