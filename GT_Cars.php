7f04127e7a9c3f17de20ca67085a3ec6fb23ecfa
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-03-22 14:31:24 -0700

    Logo Work

--- GT_Cars.php
+++ GT_Cars.php
@@ -4,7 +4,7 @@
         <title>Cult-Classic Japanese Cars: GT Cars</title>
         <link rel = "stylesheet" href = "main_cat_style.css">
         <div id = "logo_gt">
-            <img src = "Images/Logo_alter_1.jpg" alt = "Site Logo" width = "250px" height = "161px">
+            <img src = "Images/Logo_alter_1.jpg" alt = "Site Logo">
         </div>
     </head>
 
@@ -14,6 +14,8 @@
 
         <br></br>
 
+
+        <br></br>
         <p class = "gt" > A GT Car is a type of Sports Car, that is designed for high-speed, and long-distance 
           driving. GT Cars are usually longer, heavier, have a longer wheelbase than sports cars,
           Front-Engine, Rear-Wheel Drive, 2-seat or 2+2 arrangement. </p>
