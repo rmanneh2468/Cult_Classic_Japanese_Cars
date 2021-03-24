cd1aa0cd927e546e97e984653914c790823a6200
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-03-23 22:39:44 -0700

    Update Nissan 240SX (S14).php

--- Nissan 240SX (S14).php
+++ Nissan 240SX (S14).php
@@ -8,9 +8,9 @@
     <body>
 
     
-        <div class = "logo_240sx">
+    <div class = "logo_240sx">
             <img src = "Images/Logo_alter_1.jpg" alt = "Site Logo" width = "250px" height = "161px">
-        </div>
+    </div>
         
         
         <h1>1995-1998 Nissan 240sx (S14)</h1>
@@ -21,7 +21,7 @@
         <br></br>
 
         <p>Description:</p> 
-        <div id = "Twoforty_descrip">
+    <div id = "Twoforty_descrip">
             <p>The Nissan 240sx was an agile, lightweight, Rear-Wheel Drive sports car. The car was
             available, with a 5-speed manual transmission. Due to all of these factors, it gained
             a large following of spirited fans, in the racing, autocross, and drifting communities.
@@ -29,7 +29,7 @@
             cars. In fact, it is such a popular and desired car, that it is nearly impossible to find 
             a used 240sx, that has not been abused by a racer, or modded extensively. 
             </p>
-        </div>
+    </div>
         
         <br></br>
         <br></br>
