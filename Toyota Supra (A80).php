5fcb76d88bb62f85aa56db982172b6212dc1f323
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-03-25 01:44:15 -0700

    Update Toyota Supra (A80).php

--- Toyota Supra (A80).php
+++ Toyota Supra (A80).php
@@ -28,8 +28,8 @@
         fast, and good-handling machine. The Turbo version of the Mark 4 Supra, was lighter, had more 
         torque, more horsepower, better suspension, and a more sophisticated engine, (the 2JZ-GTE), than the 
         previous Supra, (the A70, Mark 3 Supra, which ran from 1986-1992, in North America). The smooth, 
-        rounded lines of the Mk4, have a very "90's look", yet still look appealing today. The Mk4 and
-        the Mk3 Toyota Supra, have nearly opposite looks, which adds to the polarizing fanbase, that both
+        rounded lines of the Mk4, have a very "90's look", yet still look appealing today. The Mk3 and
+        the Mk4 Toyota Supra, have very different designs, which adds to the polarizing fanbase, that both
         models have. The Mark 4 Supra is a legendary GT car, which generates very high prices, on the 
         used car market, and for good reason. There are few cars that can match its combination of speed, 
         handling, looks, driving experience, swagger, and great reliability.   
