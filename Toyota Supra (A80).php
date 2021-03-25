c34c24404d0cb6ddb18585a8a8944543e537809c
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-03-25 01:27:22 -0700

    Update Toyota Supra (A80).php

--- Toyota Supra (A80).php
+++ Toyota Supra (A80).php
@@ -24,15 +24,14 @@
     <div id = "Mk4_Supra_descrip">
         <p>Description:</p>
         
-        <p>The A80 Toyota Supra, (aka "Mark 4" Supra), was a truly special car. It was a very well-built, 
+        <p>The A80 Toyota Supra, (aka "Mark 4" Supra), was a truly special car. It was a well-built, 
         fast, and good-handling machine. The Turbo version of the Mark 4 Supra, was lighter, had more 
         torque, more horsepower, better suspension, and a more sophisticated engine, (the 2JZ-GTE), than the 
         previous Supra, (the A70, Mark 3 Supra, which ran from 1986-1992, in North America). The smooth, 
         rounded lines of the Mk4, have a very "90's look", yet still look appealing today. The Mk4 and
         the Mk3 Toyota Supra, have nearly opposite looks, which adds to the polarizing fanbase, that both
-        models have. Some Toyota Supra fans, prefer the edgy, wedge-shaped, angular, and aggressive look, 
-        of the Mark 3. Nevertheless, the Mark 4 is a legendary GT car, which generates very high prices, on 
-        the used car market, and for good reason. There are few cars that can match its combination of speed, 
+        models have. The Mark 4 Supra is a legendary GT car, which generates very high prices, on the 
+        used car market, and for good reason. There are few cars that can match its combination of speed, 
         handling, looks, driving experience, swagger, and great reliability.   
         </p>
     </div>
