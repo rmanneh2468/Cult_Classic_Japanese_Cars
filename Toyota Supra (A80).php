6466530307be15faca8de31e6ffb95a13e97bc89
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-03-25 01:16:17 -0700

    Made a page for GT Cars
    Mark 4 Toyota Supra page created.

---
+++ Toyota Supra (A80).php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cult-Classic Japanese Cars: Sports Cars</title>
+        <link rel = "stylesheet" href = "specific_car_style.css">
+    </head>
+
+    <body>
+
+    
+    <div class = "logo_supra_mk4">
+            <img src = "Images/Logo_alter_1.jpg" alt = "Site Logo" width = "250px" height = "161px">
+    </div>
+        
+        
+        <h1>1993-1998 Toyota Supra (A80)</h1>
+
+        <br></br>
+        <img src = https://www.cnet.com/a/img/d1Blf9YiIXWEolLNLgascZ1rK0A=/980x0/2017/11/15/2cca8d64-4cb9-4289-bb83-1bfa76a4c202/1994-toyota-supra-turbo-1.jpg  id = "Mk4_Supra_img" />
+
+        <br></br>
+
+        
+    <div id = "Mk4_Supra_descrip">
+        <p>Description:</p>
+        
+        <p>The A80 Toyota Supra, (aka "Mark 4" Supra), was a truly special car. It was a very well-built, 
+        fast, and good-handling machine. The Turbo version of the Mark 4 Supra, was lighter, had more 
+        torque, more horsepower, better suspension, and a more sophisticated engine, (the 2JZ-GTE), than the 
+        previous Supra, (the A70, Mark 3 Supra, which ran from 1986-1992, in North America). The smooth, 
+        rounded lines of the Mk4, have a very "90's look", yet still look appealing today. The Mk4 and
+        the Mk3 Toyota Supra, have nearly opposite looks, which adds to the polarizing fanbase, that both
+        models have. Some Toyota Supra fans, prefer the edgy, wedge-shaped, angular, and aggressive look, 
+        of the Mark 3. Nevertheless, the Mark 4 is a legendary GT car, which generates very high prices, on 
+        the used car market, and for good reason. There are few cars that can match its combination of speed, 
+        handling, looks, driving experience, swagger, and great reliability.   
+        </p>
+    </div>
+        
+        <br></br>
+        <br></br>
+
+        <ol>
+            <li><a href = "GT_Cars.php"> Return to the GT Cars page </a></li>
+            <li><a href = "index.php"> Return Home </a></li>
+        </ol>
+
+
+
+    </body>
+</html>
