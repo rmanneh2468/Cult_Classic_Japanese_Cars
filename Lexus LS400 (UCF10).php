bd2f387053202683aec648bbcf5ac960fe82a622
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-03-27 15:13:42 -0700

    Update Lexus LS400 (UCF10).php

--- Lexus LS400 (UCF10).php
+++ Lexus LS400 (UCF10).php
@@ -24,7 +24,29 @@
     <div id = "Lex_LS400_descrip">
         <p>Description:</p>
         
-        <p>The 1st generation, Lexus LS400, was a car  
+        <p>The 1st generation, Lexus LS400, was a car that took nearly a decade to make. The project 
+        started in 1983, by Eiji Toyoda. It was called the F1 "Flagship No. 1 vehicle", or "Circle F"
+        project. The F1 project, did not have a time scale for completion, nor did it have budget 
+        constraints. 
+        <br></br>
+        As a result, the car was completely new, as it did not use parts from the existing Toyota 
+        part bin. Furthermore, the final cost of development, for this ambitious luxury car, was over
+        $1 Billion US dollars. The car launched under a new name, "Lexus", which was created by Toyota,
+        to convey the launch of this sedan. 
+        <br></br>
+        The LS400 had an all-new, V8 engine, the 1UZ-Fe. This engine is legendary
+        for its smoothness, refined power, and excellent reliability. In fact, the engine is so impressive,
+        that it was one of the first car engines, approved by the FAA, for use in aircraft. The car also 
+        came equipped with a very robust, and long-lasting automatic transmission. The chassis had an 
+        independent, double-wishbone suspension, or optional air suspension system. 
+        <br></br>
+        The 1st gen LS400, had quieter cabin, than virtually all of the luxury car competitors.
+        Furthermore, Toyota set the price at $35,000 USD, undercutting the competition in the luxury 
+        market. The build quality of these cars is so impressive, and their reliability is very good. 
+        They pass 300,000 miles, on original enine and transmission, very often. The 1st gen Lexus
+        LS400, generated a shockwave in the automotive industry, in the early 90's. Companies like
+        BMW and Mercedes, were severely impacted, by the sucess of this iconic car. In fact, it took
+        several years, for BMW and Mercedes, to recover from the shockwave, of the 1st gen Lexus LS400.    
         </p>
     </div>
         
