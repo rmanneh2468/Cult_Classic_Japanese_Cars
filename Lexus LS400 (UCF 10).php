bc33782ff9212c1207ceafa239d81405e3d7e8c6
Ryan Manneh <ryanmanneh@u.boisestate.edu>
2021-03-27 14:21:49 -0700

    Created a car page in the Luxury Cars section
    LS400

---
+++ Lexus LS400 (UCF 10).php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cult-Classic Japanese Cars: Sports Cars</title>
+        <link rel = "stylesheet" href = "specific_car_style.css">
+    </head>
+
+    <body>
+
+    
+    <div class = "logo_lex_ls400">
+            <img src = "Images/Logo_alter_1.jpg" alt = "Site Logo" width = "250px" height = "161px">
+    </div>
+        
+        
+        <h1>1990-1994 Lexus LS400</h1>
+
+        <br></br>
+        <img src = https://www.mad4wheels.com/img/free-car-images/mobile/13055/lexus-ls-400-ucf10-usa-version-1989-386610.jpg  id = "Lex_LS400_img" />
+
+        <br></br>
+
+        
+    <div id = "Lex_LS400_descrip">
+        <p>Description:</p>
+        
+        <p>The A80 Toyota Supra, (aka "Mark 4" Supra), was a truly special car. It was a well-built, 
+        fast, and good-handling machine. The Turbo version of the Mark 4 Supra, was lighter, had more 
+        torque, more horsepower, better suspension, and a more sophisticated engine, (the 2JZ-GTE), than the 
+        previous Supra, (the A70, Mark 3 Supra, which ran from 1986-1992, in North America). The smooth, 
+        rounded lines of the Mk4, have a very "90's look", yet still look appealing today. The Mk3 and
+        the Mk4 Toyota Supra, have very different designs, which adds to the polarizing fanbase, that both
+        models have. The Mark 4 Supra is an iconic and legendary GT car, that generates very high prices, on the 
+        used car market. Some people are surprised by the high prices, that these cars sell for. Yet
+        there are few cars, that can match it's combination of speed, handling, looks, driving 
+        experience, and great reliability.   
+        </p>
+    </div>
+        
+        <br></br>
+        <br></br>
+
+        <ol>
+            <li><a href = "Luxury_Cars.php"> Return to the GT Cars page </a></li>
+            <li><a href = "index.php"> Return Home </a></li>
+        </ol>
+
+
+
+    </body>
+</html>
